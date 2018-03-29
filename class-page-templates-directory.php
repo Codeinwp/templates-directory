cdf42fab2dedeada18f12944ad01967c752dbe3d
Andrei Baicus <baicusandrei@me.com>
2018-03-29 10:02:41 +0300

    Bump version.

--- class-page-templates-directory.php
+++ class-page-templates-directory.php
@@ -14,7 +14,7 @@ if ( ! class_exists( '\ThemeIsle\PageTemplatesDirectory' ) ) {
 		 * The version of this library
 		 * @var string
 		 */
-		public static $version = '1.0.1';
+		public static $version = '1.0.2';
 
 		/**
 		 * Holds the module slug.
