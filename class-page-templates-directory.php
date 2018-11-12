a0aaae24235a3bfc21984330559789ffd70af445
Andrei Baicus <baicusandrei@me.com>
2018-11-12 11:08:24 +0200

    chore: version bump

--- class-page-templates-directory.php
+++ class-page-templates-directory.php
@@ -14,7 +14,7 @@ if ( ! class_exists( '\ThemeIsle\PageTemplatesDirectory' ) ) {
 		 * The version of this library
 		 * @var string
 		 */
-		public static $version = '1.0.7';
+		public static $version = '1.0.8';
 
 		/**
 		 * Holds the module slug.
